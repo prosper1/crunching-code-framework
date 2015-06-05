2a946e02c987cc4e0b8ee011b6c816fd4a4946cd
Mazi Muhlari <mazi.muhlari@gmail.com>
2015-06-05 22:23:54 +0200

    Made Client War

--- com.mobilebulletin/com.mobilebulliten.maven.frontend/net.mobilebulletin.spring/src/main/java/net/mobilebulletin/spring/Application.java
+++ com.mobilebulletin/com.mobilebulliten.maven.frontend/net.mobilebulletin.spring/src/main/java/net/mobilebulletin/spring/Application.java
@@ -2,6 +2,8 @@ package net.mobilebulletin.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 /*
  * @author Mazi Muhlari
@@ -15,7 +17,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(Application.class);
+	}
 	
 	public static void main(String[] args){
 		SpringApplication.run(Application.class, args);
